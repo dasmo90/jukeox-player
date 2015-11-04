655590b329963e6fe0c5650e8b73a8a9ff71b593
dasmo90 <dasmo90@gmail.com>
2015-11-04 21:01:39 +0100

    Initialized project.

---
+++ src/main/java/de/dasmo90/jukeox/player/model/JukeOxPlayer.java
@@ -0,0 +1,16 @@
+package de.dasmo90.jukeox.player.model;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by dasmo90 on 04.11.2015.
+ */
+public class JukeOxPlayer {
+
+	private static final Logger LOGGER = Logger.getLogger(JukeOxPlayer.class);
+
+	public static void main(String[] args) {
+
+		LOGGER.info("JukeOx Player started!");
+	}
+}
---
+++ src/test/java/de/dasmo90/jukeox/player/model/JukeOxPlayerTest.java
@@ -0,0 +1,8 @@
+package de.dasmo90.jukeox.player.model;
+
+/**
+ * Created by dasmo90 on 04.11.2015.
+ */
+public class JukeOxPlayerTest {
+
+}
